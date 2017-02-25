6b57e62114ff4f5925f71f9b9f2e0383ddbd8235
Chase Allen <cjan8c@mail.missouri.edu>
2017-02-25 01:36:03 -0600

    creating a new branch

--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
 var Alexa = require('alexa-sdk');
 
+//creates an Alexa object
 exports.handler = function(event, context, callback){
 	var alexa = Alexa.handler(event, context);
 }
+
+//
+var handlers = {
+
+    'HelloWorldIntent': function () {
+        this.emit(':tell', 'Hello World!');
+    }
+
+};
