cffb56b09cb4cb00be63d5eeff14d615e8cc3ad8
Chase Allen <cjan8c@mail.missouri.edu>
2017-02-25 04:52:34 -0600

    Fixing my mistakes

--- src/index.js
+++ src/index.js
@@ -1,43 +1,27 @@
 'use strict'
 
-var APP_ID = undefined;
-var Alexa = require('./alexa-sdk');
+var APP_ID = 'amzn1.ask.skill.36b93194-b1d4-4479-91c0-37e909e2ecca';
+var Alexa = require('./AlexaSkill');
+var OUTPUT = "You will probably die";
 
-//creates an Alexa object
-exports.handler = function(event, context, callback){
-	var alexa = Alexa.handler(event, context);
-	alexa.registerHandlers(handlers);
-	alexa.execute();
-}
-
-//
-var handlers = {
-
-	'OpenSymptomCheckerIntent': function(){
-		this.emit(':tell', 'Welcome to symptom checker. please list your symptoms.')
-	},
-
-    'GetDiagnonisIntent': function() {
-        this.emit(':tell', 'You are gonna die');
-    }
-
-};
-/*
-var states = {
-
-	SYMPTOMMODE: '_SYMPTOMMODE';,
-
+var ResponseService = function(){
+	Alexa.call(this, APP_ID);
 };
 
-var symptomModeHandler = Alexa.CreateStateHandler(states.SYMPTOMMODE, {
+ResponseService.prototype = Object.create(Alexa.prototype);
 
-	'SymptomSession': function(){
-		this.handler.state = '';
-		this.emitWithState('New Session');
-	},
-
-	'SymptomIntent': function(){
-		var guestSaid = this.event.
-	}
+var responseFunction = function(intent, session, response){
+	response.tell(OUTPUT);
 }
-*/
+
+ResponseService.prototype.eventHandlers.onLaunch = responseFunction;
+
+ResponseService.prototype.intentHandlers = {
+
+	'GetDiagnosisIntent' : responseFunction
+};
+
+exports.handler = function(event, context){
+	var responseService = new ResponseService();
+	responseService.execute(event,context);
+}
