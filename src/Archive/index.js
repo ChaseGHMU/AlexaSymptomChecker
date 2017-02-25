a3fe2a24443a03667f2ee23d5528eca387d40aa1
Chase Allen <cjan8c@mail.missouri.edu>
2017-02-25 05:01:23 -0600

    fixing alexa text

--- src/Archive/index.js
+++ src/Archive/index.js
@@ -2,7 +2,7 @@
 
 var APP_ID = 'amzn1.ask.skill.36b93194-b1d4-4479-91c0-37e909e2ecca';
 var Alexa = require('./AlexaSkill');
-var OUTPUT = "Welcome to symptom checker. Please list your symptoms.";
+var OUTPUT = "You will probably die";
 
 var ResponseService = function(){
 	Alexa.call(this, APP_ID);
@@ -14,15 +14,11 @@ var responseFunction = function(intent, session, response){
 	response.tell(OUTPUT);
 }
 
-var diagnosisFunction = function(intent,session,response){
-	response.tell('You will probably die');
-}
-
 ResponseService.prototype.eventHandlers.onLaunch = responseFunction;
 
 ResponseService.prototype.intentHandlers = {
 
-	'GetDiagnosisIntent' : diagnosisFunction;
+	'GetDiagnosisIntent' : responseFunction
 };
 
 exports.handler = function(event, context){
