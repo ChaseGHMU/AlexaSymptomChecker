e7efca20880ffc851d840ec85c411dde2f0a9270
Chase Allen <cjan8c@mail.missouri.edu>
2017-02-26 00:27:30 -0600

    JSON working, Alexa responding

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
-'use strict'
+'use strict';
 
-var APP_ID = 'amzn1.ask.skill.3b8ded99-c4c3-43a3-ad0e-2d9d86484dc4';
+var APP_ID = 'amzn1.ask.skill.6a08a1ad-ca92-4cf6-8b33-eb63a7e3cc6a';
 
 var request = require('request');
-var IMO = require('./IMO');
 var Alexa = require('./AlexaSkill');
 
 var OUTPUT = "Welcome to symptom checker. Please list your symptoms.";
@@ -13,69 +12,66 @@ var OUTPUT = "Welcome to symptom checker. Please list your symptoms.";
                 apiKeySecret: "YzQwNWViZTMxNDQwNGNlNWJlZjEzMmU2MWU5YzMxZGQ6N0Q4MjJBRjUxMERCMzZERDlGQzQ5NTVENDRBMDUyMjkwNzMxNjFFODU2OUI5QUUwRjFERjk1Q0ZGOTI2NjMyRQ==",
                 product: "ProblemIT_Professional/search"
 }*/
-exports.handler = function(event,context){
-  var responseService = new ResponseService();
-  responseService.execute(event,context);
-};
 
 var ResponseService = function(){
 	Alexa.call(this, APP_ID);
-}
+};
 
 ResponseService.prototype = Object.create(Alexa.prototype);
 
 var responseFunction = function(intent, session, response){
 	response.tell(OUTPUT);
-}
+  console.log(response);
+  console.log(intent);
+  console.log(session);
+};
 
 var diagnosisFunction = function(intent, session, response){
-	console.log(intent);
-	console.log(intent.slots.system.value);
 
-}
+  var postData = {
+       searchTerm: intent.slots.symptom.value,
+       numberOfResults: 5,
+       clientApp: "TestApp",
+       clientAppVersion:  "0.0.1",
+       siteId: "site",
+       userId: "user"
+   };
+   var url = 'http://184.73.124.73/PortalWebService/api/v2/product/problemIT_Professional/search';
+   var options = {
+       method: 'post',
+       body: postData,
+       json: true,
+       url: url,
+       headers: {
+           'Authorization': 'Basic YzQwNWViZTMxNDQwNGNlNWJlZjEzMmU2MWU5YzMxZGQ6N0Q4MjJBRjUxMERCMzZERDlGQzQ5NTVENDRBMDUyMjkwNzMxNjFFODU2OUI5QUUwRjFERjk1Q0ZGOTI2NjMyRQ=='
+       }
+   };
+   request(options, function (err, res, body) {
+       if (err) {
+           console.log('error posting json', err);
+       } else {
+           var headers = res.headers;
+           var statusCode = res.statusCode;
+           console.log('headers', headers);
+           console.log('statusCode', statusCode);
+           console.log('body', body);
+           response.tell('the description of ' + intent.slots.symptom.value + ' is ' + body.SearchTermResponse.items[0].ICD10CM_TITLE);
+       }
+   });
+};
 
-exports.handler = (event, context, callback) => {
-    var postData = {
-        searchTerm: "diabetes type 2",
-        numberOfResults: 5,
-        clientApp: "TestApp",
-        clientAppVersion:  "0.0.1",
-        siteId: "site",
-        userId: "user"
-    }
-    var url = 'http://184.73.124.73/PortalWebService/api/v2/product/problemIT_Professional/search'
-    var options = {
-        method: 'post',
-        body: postData,
-        json: true,
-        url: url,
-        headers: {
-            'Authorization': 'Basic YzQwNWViZTMxNDQwNGNlNWJlZjEzMmU2MWU5YzMxZGQ6N0Q4MjJBRjUxMERCMzZERDlGQzQ5NTVENDRBMDUyMjkwNzMxNjFFODU2OUI5QUUwRjFERjk1Q0ZGOTI2NjMyRQ=='
-        }
-    }
-    request(options, function (err, res, body) {
-        if (err) {
-            console.log('error posting json', err)
-            callback(err);
-        } else {
-            var headers = res.headers
-            var statusCode = res.statusCode
-            console.log('headers', headers)
-            console.log('statusCode', statusCode)
-            console.log('body', body)
-            callback(null, body.SearchTermResponse.items[0].title);
-        }
-    })
+exports.handler = function(event,context){
+  var responseService = new ResponseService();
+  responseService.execute(event,context);
 };
 
 var helpFunction = function(intent,session,response){
 	response.tell('Just tell me your symptoms and I will find a diagnosis for you.');
-}
+};
 
 ResponseService.prototype.eventHandlers.onLaunch = responseFunction;
 
 ResponseService.prototype.intentHandlers = {
-
 	'GetDiagnosisIntent' : diagnosisFunction,
 	'HelpIntent' : helpFunction
 };
